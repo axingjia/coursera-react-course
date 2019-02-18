d8209297d04db2c41c44339627a7432127b9178c
Alan Huang <axingjia@gmail.com>
2019-02-17 18:38:59 -0800

    SPA

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody,
-    CardTitle } from 'reactstrap';
+// import { Card, CardImg, CardImgOverlay, CardText, CardBody,
+//     CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 class Dishdetail extends Component {
 
@@ -44,22 +47,34 @@ class Dishdetail extends Component {
         
       }else{
           return (
-            <div className="container">
-            <div className="row">
-                <Card className="col-12 col-md-5 m-1">
-                  <CardImg top src={dish.image} alt={dish.name} />
-                  <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                  </CardBody>
-              </Card>
-              <div className="col-12 col-md-5 m-1">
-                {this.renderComments(dish.comments)}
-              </div>
-            </div>
-            </div>
-            
-        );
+                 <div className="container">
+                 <div className="row">
+                     <Breadcrumb>
+
+                         <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                         <BreadcrumbItem active>{this.props.dish.name}</BreadcrumbItem>
+                     </Breadcrumb>
+                     <div className="col-12">
+                         <h3>{this.props.dish.name}</h3>
+                         <hr />
+                     </div>                
+                 </div>
+                 <div className="row">
+                     <div className="col-12 col-md-5 m-1">
+                     <Card className="col-12 col-md-5 m-1">
+                          <CardImg top src={dish.image} alt={dish.name} />
+                          <CardBody>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
+                          </CardBody>
+                      </Card>
+                     </div>
+                     <div className="col-12 col-md-5 m-1">
+                         {this.renderComments(dish.comments)}
+                     </div>
+                 </div>
+                 </div>
+             );
     }
 
   }
